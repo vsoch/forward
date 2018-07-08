f6951a4c4ab8f2ed4fe711bbfef7ab1b36316051
Raphael Townshend <raphael@tc.com>
2018-07-08 14:46:19 -0700

    Clarify setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,29 @@
 #
 # Sets up parameters for use with other scripts.  Should be run once.
 # Sample usage: bash setup.sh
-read -p "Sherlock username: "  USERNAME
+echo
+read -p "Sherlock username > "  USERNAME
 
-read -p "Port to use (pick a random number in range 49152-65535): "  PORT
+echo
+echo "Next, pick a port to use.  If someone else is port forwarding using that
+port already, this script will not work.  If you pick a random number in the
+range 49152-65335, you should be good."
+echo
+read -p "Port to use > "  PORT
 
-read -p "Sherlock partition (default: rondror): "  PARTITION
-PARTITION=${PARTITION:-rondror}
+echo
+echo "Next, pick the sherlock partition on which you will be running your
+notebooks.  If your PI has purchased dedicated hardware on sherlock, you can use
+that partition.  Otherwise, leave blank to use the default partition (normal)."
+echo
+read -p "Sherlock partition (default: normal) > "  PARTITION
+PARTITION=${PARTITION:-normal}
 
-read -p "Browser to use (default: /Applications/Safari.app/): "  BROWSER
-BROWSER=${partition:-"/Applications/Safari.app/"}
+echo
+echo "Next, pick the path to the browser you wish to use.  Will default to Safari."
+echo
+read -p "Browser to use (default: /Applications/Safari.app/) > "  BROWSER
+BROWSER=${BROWSER:-"/Applications/Safari.app/"}
 
 MEM=20G
 
