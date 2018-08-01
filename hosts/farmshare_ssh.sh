eaef66f93d3e951390f3d83f7bf9cc15dca7718c
Vanessa Sochat <vsochat@stanford.edu>
2018-08-01 15:31:12 -0700

    tweak to farmshare ssh

--- hosts/farmshare_ssh.sh
+++ hosts/farmshare_ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Sherlock cluster at Stanford
+# Farmshare cluster at Stanford
 # Prints an ssh configuration for the user, selecting a login node at random
-# Sample usage: bash sherlock_ssh.sh
+# Sample usage: bash farmshare_ssh.sh
 echo
 read -p "Farmshare username > "  USERNAME
 
