d7396d3ab75a401e9a18c2f39ae880f0bb6feb70
Vanessa Sochat <vsochat@stanford.edu>
2018-08-16 17:59:39 -0400

    fixing bug with resume!

--- resume.sh
+++ resume.sh
@@ -10,7 +10,10 @@ then
 fi
 source params.sh
 
+NAME="${1}"
+
 # The user is required to specify port
 
+echo "ssh ${RESOURCE} squeue --name=$NAME --user=$USERNAME -o "%N" -h"
 MACHINE=`ssh ${RESOURCE} squeue --name=$NAME --user=$USERNAME -o "%N" -h`
 ssh -L $PORT:localhost:$PORT ${RESOURCE} ssh -L $PORT:localhost:$PORT -N $MACHINE &
